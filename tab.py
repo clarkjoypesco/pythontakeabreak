b3a76406dec889ac7b0f58d816b7c44efaf48008
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-14 09:30:30 +0800

    use time and webbrowser with loop

--- tab.py
+++ tab.py
@@ -1,1 +1,11 @@
-webbrowser.open('https://www.youtube.com/watch?v=FMwMg7dyfDk')
+import time
+import webbrowser
+
+total_breaks = 3
+break_count = 0
+
+print 'This program started on ' + time.ctime()
+while break_count < total_breaks:
+    time.sleep(10)
+    webbrowser.open('https://www.youtube.com/watch?v=XsX3ATc3FbA')
+    break_count+=1
